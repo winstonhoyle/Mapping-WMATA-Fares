e47a3355c74555ea6264aad7d82266d6c779b0af
Winston Hoyle <hoylejwinston@gmail.com>
2018-12-10 19:46:06 -0500

    Python
    Python needed to get and clean data

---
+++ Python/ConvertToJSON.py
@@ -0,0 +1,22 @@
+import csv
+import json
+from collections import defaultdict
+import GetField
+
+jsonformat = defaultdict(list)
+
+##Open csv
+csvfile = open('all_stations.csv', newline='')
+filereader = csv.reader(csvfile,delimiter=",",quotechar="|")
+##Skip column names
+next(filereader, None)
+
+##Loop through every row appending station and fare info
+for row in filereader:
+    jsonformat[row[0]].append({"station":row[1], "fares": [ {"peak":row[2],"offpeak":row[3],"reduced_peak":row[4],"reduced_offpeak":row[5] }] } )
+
+csvfile.close()
+
+##Create a json file
+with open("all_stations.json", "w") as jsonfile:
+    json.dump(jsonformat,jsonfile,indent=1)
